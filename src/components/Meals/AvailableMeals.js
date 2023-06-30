0705516f6befe2a1dc4677e5543051708b96305a
d5ng <dong5ffice@gmail.com>
2023-06-30 13:47:37 +0900

    Error handling

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,51 +1,58 @@
 import Card from "../UI/Card"
 import MealItem from "./MealItem/MealItem"
 import classes from "./AvailableMeals.module.css"
-import { useEffect, useState, useCallback } from "react"
+import { useEffect, useState } from "react"
 
 const FIREBASE_KEY = process.env.REACT_APP_FIREBASE_KEY
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([])
   const [isLoading, setIsLoading] = useState(true)
-  const [error, setError] = useState(null)
-
-  const fetchMeals = useCallback(async () => {
-    setIsLoading(true)
-    setError(false)
-
-    try {
-      const response = await fetch(FIREBASE_KEY)
-
-      if (!response.ok) throw new Error("Response Not Ok")
-
-      const responseData = await response.json()
-      const loadedMeals = []
-
-      for (const meal in responseData) {
-        loadedMeals.push({
-          id: meal,
-          name: responseData[meal].name,
-          description: responseData[meal].description,
-          price: responseData[meal].price,
-        })
-      }
-
-      setMeals(loadedMeals)
-    } catch (error) {
-      setError(error.message)
-    }
-    setIsLoading(false)
-  }, [])
+  const [httpError, setHttpError] = useState(null)
 
   useEffect(() => {
+    const fetchMeals = async () => {
+      setIsLoading(true)
+      try {
+        const response = await fetch(FIREBASE_KEY)
+
+        if (!response.ok) throw new Error("Response Not Ok")
+
+        const responseData = await response.json()
+        const loadedMeals = []
+
+        for (const meal in responseData) {
+          loadedMeals.push({
+            id: meal,
+            name: responseData[meal].name,
+            description: responseData[meal].description,
+            price: responseData[meal].price,
+          })
+        }
+
+        setMeals(loadedMeals)
+        setIsLoading(false)
+      } catch (error) {
+        setHttpError(error.message)
+      }
+      setIsLoading(false)
+    }
+
     fetchMeals()
-  }, [fetchMeals])
+  }, [])
 
   if (isLoading) {
     return (
       <section className={classes.MealsLoading}>
         <p>Loading...</p>
+      </section>
+    )
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     )
   }
