693cbf545c9047755325ba577ce6effde1d4637f
d5ng <dong5ffice@gmail.com>
2023-06-30 13:30:22 +0900

    http fetch meal

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,44 +1,40 @@
-import Card from '../UI/Card';
-import MealItem from './MealItem/MealItem';
-import classes from './AvailableMeals.module.css';
+import Card from "../UI/Card"
+import MealItem from "./MealItem/MealItem"
+import classes from "./AvailableMeals.module.css"
+import { useEffect, useState, useCallback } from "react"
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+const FIREBASE_KEY = process.env.REACT_APP_FIREBASE_KEY
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem
-      key={meal.id}
-      id={meal.id}
-      name={meal.name}
-      description={meal.description}
-      price={meal.price}
-    />
-  ));
+  const [meals, setMeals] = useState([])
+
+  const fetchMeals = useCallback(async () => {
+    const response = await fetch(FIREBASE_KEY)
+    const responseData = await response.json()
+
+    const loadedMeals = []
+
+    for (const meal in responseData) {
+      loadedMeals.push({
+        id: meal,
+        name: responseData[meal].name,
+        description: responseData[meal].description,
+        price: responseData[meal].price,
+      })
+    }
+
+    setMeals(loadedMeals)
+  }, [])
+
+  useEffect(() => {
+    fetchMeals()
+  }, [fetchMeals])
+
+  console.log(meals)
+
+  const mealsList = meals.map((meal) => (
+    <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} />
+  ))
 
   return (
     <section className={classes.meals}>
@@ -46,7 +42,7 @@ const AvailableMeals = () => {
         <ul>{mealsList}</ul>
       </Card>
     </section>
-  );
-};
+  )
+}
 
-export default AvailableMeals;
+export default AvailableMeals
